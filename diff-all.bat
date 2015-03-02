9f5743f9d6dadcc3f75a099ba6f42ccb6bde338e
Tetsuo Koyama <tkoyama010@gmail.com>
2015-03-02 18:37:07 +0900

    diff-xls.jsを追加
    Excelファイルに対応

--- diff-all.bat
+++ diff-all.bat
@@ -2,6 +2,8 @@ if %~x1 EQU .docx (
 wscript.exe "C:\Program Files\TortoiseGit\Diff-Scripts\diff-doc.js" %1 %2 //E:javascript
 ) else if %~x1 EQU .xlsx (
 wscript.exe "C:\Program Files\TortoiseGit\Diff-Scripts\diff-xls.js" %1 %2 //E:javascript
+) else if %~x1 EQU .xls (
+wscript.exe "C:\Program Files\TortoiseSVN\Diff-Scripts\diff-xls.js" %1 %2 //E:javascript
 ) else (
 "C:\Program Files\WinMerge\WinMergeU.exe" %1 %2
 )
